b4f7eb1448d27c1e091171fbf164ab58e50174ce
RITURAJGHOSH2000 <107643300+RITURAJGHOSH2000@users.noreply.github.com>
2022-06-18 19:47:27 +0530

    Add files via upload

---
+++ Python Introduction/Our First Python Program.py
@@ -0,0 +1,22 @@
+"""
+    Created by Rituraj Ghosh on 13/06/2022
+"""
+1.
+print('Rituraj Ghosh') 
+# Rituraj Ghosh
+
+2.
+input('What is your name?') 
+# What is your name? Rituraj Ghosh 
+
+3.
+name = input('What is your name?')
+print(name) 
+# What is your name? Rituraj Ghosh
+# Rituraj Ghosh
+
+4.
+name = input('What is ypur name?')
+print('Your name is:', name)
+# What is your name? Rituraj Ghosh
+# Your name is: Rituraj Ghosh
