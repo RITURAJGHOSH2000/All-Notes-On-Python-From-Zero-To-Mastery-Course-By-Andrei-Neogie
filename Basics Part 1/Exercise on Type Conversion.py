78aa468e52a626c885af775d03aaaff1662b5f6b
RITURAJGHOSH2000 <107643300+RITURAJGHOSH2000@users.noreply.github.com>
2022-06-19 11:26:02 +0530

    Add files via upload

---
+++ Basics Part 1/Exercise on Type Conversion.py
@@ -0,0 +1,7 @@
+"""
+    Created by Rituraj Ghosh on 19/06/2022
+"""
+# Exercise: Create an age calculator
+birth_year = input('What is your birth year? \n')
+age = 2022 - int(birth_year)
+print(f'Your age is: {age}') 
