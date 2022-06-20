5bfbc7d8a2f217983b4d7235ca03ffcfc6205c40
RITURAJGHOSH2000 <107643300+RITURAJGHOSH2000@users.noreply.github.com>
2022-06-20 10:19:29 +0530

    Add files via upload

---
+++ Basics Part 1/List.py
@@ -0,0 +1,8 @@
+"""
+    Created by Rituraj Ghosh on 20/06/2022
+"""
+# list
+li = ['a', 'b', 'c']
+li2 = [1, 2, 3]
+amazon_cart = ['notebooks', 'sunglasses']
+# These all are examples of lists, which is similar to array in other programming language
