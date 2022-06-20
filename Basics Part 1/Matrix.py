021dafdac9579b834d9101136414f8fcf8a5376f
RITURAJGHOSH2000 <107643300+RITURAJGHOSH2000@users.noreply.github.com>
2022-06-20 10:52:42 +0530

    Add files via upload

---
+++ Basics Part 1/Matrix.py
@@ -0,0 +1,7 @@
+"""
+    Created by Rituraj Ghosh on 20/06/2022
+"""
+# Matrix is a way to describe multi-dimensional list
+matrix = [ [1,2,3], [2,4,6], [3,5,7]]
+print(matrix[0][2]) # 3
+# There can be innumerable number of lists inside one matrix
