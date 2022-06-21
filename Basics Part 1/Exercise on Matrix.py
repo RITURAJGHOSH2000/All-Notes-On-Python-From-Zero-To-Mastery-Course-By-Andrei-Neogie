cefa753c541865846d6a4c4bb49d55172c1a50a8
RITURAJGHOSH2000 <107643300+RITURAJGHOSH2000@users.noreply.github.com>
2022-06-22 00:11:48 +0530

    Add files via upload

---
+++ Basics Part 1/Exercise on Matrix.py
@@ -0,0 +1,7 @@
+"""
+    Created by Rituraj Ghosh on 21/06/2022
+"""
+# Using this list:
+basket = ["Banana", ["Apples", ["Oranges"], "Blueberries"]]
+# Access "Oranges" and print it":
+print(basket[1][1]) # ["Oranges"]
