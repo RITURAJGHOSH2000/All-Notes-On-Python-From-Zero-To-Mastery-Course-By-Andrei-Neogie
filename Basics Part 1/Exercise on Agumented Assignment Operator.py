85d773792bb0cd01dc102a3696ff1a859f77e768
RITURAJGHOSH2000 <107643300+RITURAJGHOSH2000@users.noreply.github.com>
2022-06-21 23:45:10 +0530

    Add files via upload

---
+++ Basics Part 1/Exercise on Agumented Assignment Operator.py
@@ -0,0 +1,13 @@
+"""
+    Created by Rituraj Ghosh on 21/06/2022
+"""
+counter = 0
+counter += 1
+counter += 1
+counter += 1
+counter += 1
+counter -= 1
+counter *= 2
+
+# Before you click RUN, guess what is the value of the counter variable in memory!
+print(counter) # 6
