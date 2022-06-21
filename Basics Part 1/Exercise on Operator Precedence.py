26886d960b65685f29946dacb6b2ca3c86ec46d0
RITURAJGHOSH2000 <107643300+RITURAJGHOSH2000@users.noreply.github.com>
2022-06-21 23:39:54 +0530

    Add files via upload

---
+++ Basics Part 1/Exercise on Operator Precedence.py
@@ -0,0 +1,10 @@
+"""
+    Created by Rituraj Ghosh on 21/06/2022
+"""
+# Guess the output of each answer before you click RUN
+# Try to write down your answer before and see how you do... keep it in mind I made it a little tricky for you :)
+print((5+4)*10/2) #45.0
+print(((5+4)*10)/2) # 45.0
+print((5+4)*(10/2)) # 45.0
+print(5+(4*10)/2) # 25.0
+print(5+4*10//2) # 25
