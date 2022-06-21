633867cdf3029effea43b0f613908629642885a5
RITURAJGHOSH2000 <107643300+RITURAJGHOSH2000@users.noreply.github.com>
2022-06-21 17:56:28 +0530

    Update Dictionary.py

--- Basics Part 1/Dictionary.py
+++ Basics Part 1/Dictionary.py
@@ -1,6 +1,6 @@
 """
     Created by Rituraj Ghosh on 20/06/2022
 """
-# Dictionary is an unordered key value pair
+# Dictionary is an unordered key value pair and it is not sorted like the list
 dictionary = {'a':1,'b':2}
 print(dictionary['a'])
