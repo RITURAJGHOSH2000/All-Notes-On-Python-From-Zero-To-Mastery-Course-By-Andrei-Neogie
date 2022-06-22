a6ec6f6011c88092997c1d6f21779fd1a66f3a28
RITURAJGHOSH2000 <107643300+RITURAJGHOSH2000@users.noreply.github.com>
2022-06-22 18:27:06 +0530

    Add files via upload

---
+++ Basics Part 2/Ternary Operator.py
@@ -0,0 +1,7 @@
+"""
+    Created by Rituraj Ghosh on 22/06/2022
+"""
+# Ternary Operator, here the 6th line is ternary operator which makes the code smaller and look premium
+is_friend = True
+can_message = "message allowed" if is_friend else "not allowed to message"
+print(can_message) # message allowed
