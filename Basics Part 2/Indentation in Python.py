1ab3a5a0e13a69574b66160033d8c7094203afab
RITURAJGHOSH2000 <107643300+RITURAJGHOSH2000@users.noreply.github.com>
2022-06-22 18:06:27 +0530

    Add files via upload

---
+++ Basics Part 2/Indentation in Python.py
@@ -0,0 +1,11 @@
+"""
+    Created by Rituraj Ghosh on 22/06/2022
+"""
+is_old = True
+is_licenced = True
+if is_old and is_licenced:
+    print('you are old enough to drive, and you have a licence!')
+else:
+    print('you are not of age!')
+print('okok')
+# Here as we press enter after writting the condition in the if line there python automatically gives a space which makes the interpretor recognize that the below written statements are under the conditional logic
