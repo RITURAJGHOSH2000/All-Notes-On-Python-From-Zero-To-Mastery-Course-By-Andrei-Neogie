b95043d8a7de680ce61d0b790139edef055c92c2
RITURAJGHOSH2000 <107643300+RITURAJGHOSH2000@users.noreply.github.com>
2022-06-22 17:54:02 +0530

    Add files via upload

---
+++ Basics Part 2/Conditional Logic.py
@@ -0,0 +1,10 @@
+"""
+    Created by Rituraj Ghosh on 22/06/2022
+"""
+is_old = True
+is_licenced = True
+if is_old:
+    print('you are old enough to drive!')
+else:
+    print('checheck') # you are old enough to drive!       Now if we set the value of is_old to false then the print statement under the if section will be skipped and the print statement under the else section will be printed
+# elif can be added in the if else condition where there is more than 2 conditions present and it can be added limitlessly 
