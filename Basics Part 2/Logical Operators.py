27cdf7ad68780ccc02792984d985cdc2f64d495f
RITURAJGHOSH2000 <107643300+RITURAJGHOSH2000@users.noreply.github.com>
2022-06-22 21:47:20 +0530

    Add files via upload

---
+++ Basics Part 2/Logical Operators.py
@@ -0,0 +1,13 @@
+"""
+    Created by Rituraj Ghosh on 22/06/2022
+"""
+# Logical Operators
+print(4>5) # False
+print(4==5) # False
+print(1<2<3<4<5) # True
+print(1>=0) # True
+print(1<=0) # False
+print(0!=0) # False
+and
+or
+print(not(True)) # False
