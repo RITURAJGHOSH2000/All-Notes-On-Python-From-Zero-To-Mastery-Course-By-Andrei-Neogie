6cc7c7e9d8f2fe8a03cd47c602bf5705da8b4f29
RITURAJGHOSH2000 <107643300+RITURAJGHOSH2000@users.noreply.github.com>
2022-06-22 22:28:49 +0530

    Add files via upload

---
+++ Basics Part 2/For loop.py
@@ -0,0 +1,20 @@
+"""
+    Created by Rituraj Ghosh on 22/06/2022
+"""
+for item in (1,2,3,4,5):
+    for x in ['a', 'b', 'c']:
+        print(item, x) # 1 a
+                       # 1 b
+                       # 1 c
+                       # 2 a
+                       # 2 b
+                       # 2 c
+                       # 3 a
+                       # 3 b
+                       # 3 c
+                       # 4 a
+                       # 4 b
+                       # 4 c
+                       # 5 a
+                       # 5 b
+                       # 5 c
