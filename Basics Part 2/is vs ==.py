e90f29e40c340e72e366a6e12af7bc9d3ba6e46a
RITURAJGHOSH2000 <107643300+RITURAJGHOSH2000@users.noreply.github.com>
2022-06-22 22:17:25 +0530

    Add files via upload

---
+++ Basics Part 2/is vs ==.py
@@ -0,0 +1,14 @@
+"""
+    Created by Rituraj Ghosh on 22/06/2022
+"""
+print(True==1) # True
+print(''==1) # False
+print([]==1) # False
+print(10==10.0) # True
+print([]==[]) # True
+
+print(True is 1) # False
+print('' is 1) # False
+print([] is 1) # False
+print(10 is 10.0) # False
+print([] is []) # False
