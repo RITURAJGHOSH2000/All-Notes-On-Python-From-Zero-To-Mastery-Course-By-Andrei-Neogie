4dc36126752a8d94c713d6e9f911cb789730afd9
RITURAJGHOSH2000 <107643300+RITURAJGHOSH2000@users.noreply.github.com>
2022-06-23 10:53:07 +0530

    Add files via upload

---
+++ Basics Part 2/range().py
@@ -0,0 +1,10 @@
+"""
+    Created by Rituraj Ghosh on 23/06/2022
+"""
+for number in range(0, 100, 2):
+    print(number) # The output is 0 to 100 with 2 stepover
+
+# range() creates a special type of object that we can iterate over
+
+for number in range(100, 0, -2) :
+    print(number) # The output will be from 100 to 0 with -2 stepover
