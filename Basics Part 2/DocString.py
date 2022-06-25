f7dfe1f3ce59a37b71edd3109e1f848bf01400f7
RITURAJGHOSH2000 <107643300+RITURAJGHOSH2000@users.noreply.github.com>
2022-06-25 21:30:56 +0530

    Add files via upload

---
+++ Basics Part 2/DocString.py
@@ -0,0 +1,9 @@
+"""
+    Created by Rituraj Ghosh on 25/06/2022
+"""
+def test(a):
+    '''
+    Info: this function testa and prints param a
+    '''
+    print(a)
+test() # Here as soon as we write test() it shows the line written inside the fuction, i.e., we can comment on our code and if our code is seen by another coder like when we write large codes then instead of finding what the function does, just by typing that particular function we can see what it does 
