28f9333baced455fc51fbd79a7aa86d5030dc9cf
RITURAJGHOSH2000 <107643300+RITURAJGHOSH2000@users.noreply.github.com>
2022-06-25 20:21:14 +0530

    Add files via upload

---
+++ Basics Part 2/Parameters and Arguments.py
@@ -0,0 +1,10 @@
+"""
+    Created by Rituraj Ghosh on 24/06/2022
+"""
+# Parameters
+def say_hello(name, emoji):
+    print(f'Helllooooo {name} {emoji}')
+# Arguments
+say_hello(name='Rituraj', emoji=':)') # Helllooooo Rituraj :)
+say_hello(name='Brad', emoji=':)') # Helllooooo Brad :)
+say_hello(name='Andy', emoji=':)') # Helllooooo Andy :)
