438dff17a770422b06e46b053d1b169f2d6fc3c6
RITURAJGHOSH2000 <107643300+RITURAJGHOSH2000@users.noreply.github.com>
2022-06-25 21:21:01 +0530

    Add files via upload

---
+++ Basics Part 2/Methods vs Functions.py
@@ -0,0 +1,15 @@
+"""
+    Created by Rituraj Ghosh on 25/06/2022
+"""
+# Methods vs Functions
+list()
+print()
+max()
+min()
+input()
+# These all are functions 
+.capitalize()
+.upper()
+.lower()
+.isnumeric()
+# These all are methods as they are written with '.'
