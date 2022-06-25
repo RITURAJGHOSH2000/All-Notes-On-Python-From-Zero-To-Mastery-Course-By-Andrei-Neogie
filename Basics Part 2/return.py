d3b80319ee3d8b9898136922f50666763556de5e
RITURAJGHOSH2000 <107643300+RITURAJGHOSH2000@users.noreply.github.com>
2022-06-25 20:45:00 +0530

    Add files via upload

---
+++ Basics Part 2/return.py
@@ -0,0 +1,9 @@
+"""
+    Created by Rituraj Ghosh on 25/06/2022
+"""
+def sum(num1, num2):
+    return num1 + num2
+print(sum(4,5)) # 9
+
+# A function should do one thing really well and should return something
+# As soon as the interrpretur encounters return it exits that function
